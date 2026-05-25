ebbe563326b43de6eaad403799246750251fa8f1
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-05-25 21:48:29 +0530

    chore(templates): remove redundant ui.lua wrapper

--- lua/matugen/templates/ui.lua
+++
@@ -1,8 +0,0 @@
-return function(c, hl)
-  require("matugen.templates.dressing")(c, hl)
-  require("matugen.templates.flash")(c, hl)
-  require("matugen.templates.todo_comments")(c, hl)
-  require("matugen.templates.render_markdown")(c, hl)
-  require("matugen.templates.avante")(c, hl)
-  require("matugen.templates.fzf_lua")(c, hl)
-end
