21fc80360c2d7fb7eb63a7016bde504e0ad3c119
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-07-16 21:21:20 +0530

    remove SIGUSR1 debounce timer

--- plugin/matugen.lua
+++ plugin/matugen.lua
@@ -6,26 +6,8 @@ vim.api.nvim_create_user_command("MatugenReload", function()
 	vim.notify("matugen: theme and templates reloaded")
 end, { desc = "Reload templates from disk and reapply the theme" })
 
-local _debounce_timer = nil
-local _debounce_ms = 50
-
 local signal = (vim.uv or vim.loop).new_signal()
-signal:start(
-	"sigusr1",
-	vim.schedule_wrap(function()
-		if _debounce_timer then
-			_debounce_timer:stop()
-			_debounce_timer:close()
-		end
-		_debounce_timer = vim.uv.new_timer()
-		_debounce_timer:start(_debounce_ms, 0, vim.schedule_wrap(function()
-			if _debounce_timer then
-				_debounce_timer:stop()
-				_debounce_timer:close()
-				_debounce_timer = nil
-			end
-			matugen.load_theme(false)
-		end))
-	end)
-)
+signal:start("sigusr1", vim.schedule_wrap(function()
+	matugen.load_theme(false)
+end))
 
